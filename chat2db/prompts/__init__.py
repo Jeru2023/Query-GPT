4676754756326410ccc69ca7f814cba9a3514775
namejaho <namejaho@gmail.com>
2023-07-20 15:50:06 +0800

    将 chat 功能拆分到 chat/目录下

--- chat2db/prompts/__init__.py
+++ chat2db/prompts/__init__.py
@@ -5,4 +5,3 @@ import chat2db.utils as U
 def load_prompt(prompt):
     package_path = pkg_resources.resource_filename("chat2db", "")
     return U.load_text(f"{package_path}/prompts/{prompt}.txt")
-   
