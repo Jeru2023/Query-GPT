ea5a8445153943107b64a8fa67d46ecfd0f61d3e
Jeru <8791177+jeru@user.noreply.gitee.com>
2023-07-21 14:24:32 +0800

    pretty print

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if prompt := st.chat_input("What's your question?"):
         ### print out the origin question
         st.markdown(f"Your question is: **:blue[{prompt}]**")
         message_placeholder = st.empty()
-        full_response = ""
+        analysis_response = ""
 
     azure = Azure(st.secrets["OPENAI_API_KEY"], st.secrets["OPENAI_API_BASE"], st.secrets["ENGINE"])
     azure.azure_ask(st.session_state.messages)
@@ -41,10 +41,11 @@ if prompt := st.chat_input("What's your question?"):
     # message_placeholder.markdown(full_response + "▌")
 
     for response in azure.azure_ask(st.session_state.messages):
-        full_response += response
-        message_placeholder.markdown(full_response + "▌")
-    print(full_response)
-    json_response = json.loads(full_response)
+        analysis_response += response
+        #message_placeholder.markdown(full_response + "▌")
+    #print(full_response)
+    json_response = json.loads(analysis_response)
+
     if json_response['response'].get('dbCheck') != 'Yes':
         # TODO: 不查询数据库
         pass
@@ -53,31 +54,41 @@ if prompt := st.chat_input("What's your question?"):
         keywods = json_response['response'].get('keywords')
         channel = Channel(db_name)
 
-        message_placeholder.markdown(full_response)
+        step1 = '### **Step 1: Analysis prompt generates [Analysis Context]**\n'
+        output = step1 + analysis_response
+        
         table_names = ','.join(channel.target_tables).strip(',')
         table_schemas = channel.database_schema_string
-        analysis_context = full_response
+        analysis_context = analysis_response
 
-        full_response += "\n\n"
-        full_response += f"**:red[Database name]**: {db_name}\n\n"
-        full_response += f"**:red[Table names]**: {table_names}\n\n"
-        full_response += f"**:blue[Table Schemas]**: {table_schemas}\n\n"
-        message_placeholder.markdown(full_response)
+        output += "\n\n"
+        output += f"**:red[Database name]**: {db_name}\n\n"
+        output += f"**:red[Table names]**: {table_names}\n\n"
+
+        step2 = '### **Step 2: Python code generates [Database Context]**\n'
+        output += step2 
+        output += f"**:blue[Table Schemas]**: {table_schemas}\n\n"
 
         db_system_prompt = [
             {"role": "system", "content": load_prompt("sql_gen_template").format(db_context_value=table_schemas,
                                                                                  analysis_context_value=analysis_context)},
             {"role": "user", "content": prompt},
-            {"role": "assistant", "content": full_response},
+            {"role": "assistant", "content": analysis_response},
         ]
-        full_response += str(db_system_prompt)
-        full_response += "\n\n"
 
-        message_placeholder.markdown(full_response)
+        #full_response += str(db_system_prompt)
+        #full_response += "\n\n"
+        #message_placeholder.markdown(full_response)
 
+        query_response = ''
         for response in azure.azure_ask(db_system_prompt):
-            full_response += response
-        full_response += "\n\n"
-        message_placeholder.markdown(full_response + "▌")
+            query_response += response
+
+        output += "\n\n"
+        step3 = '### **Step 3: Query prompt generates SQL based on [Database Context] and [Analysis Context]**\n'
+        output += step3
+        output += query_response
+        
+        message_placeholder.markdown(output + "▌")
 
     # st.session_state.messages.append({"role": "assistant", "content": full_response})
